22d4f4c9d684be50a455f6298278fddca8332261
tamask <tamask@metropolia.fi>
2021-02-28 18:06:51 +0200

    added UI version 1.0 for the AddRecipe activity

--- app/src/main/java/fi/group6/dessertrecipeapp/ActivityAddRecipe.java
+++ app/src/main/java/fi/group6/dessertrecipeapp/ActivityAddRecipe.java
@@ -7,11 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ActivityAddRecipe extends AppCompatActivity {
+public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +30,18 @@ public class ActivityAddRecipe extends AppCompatActivity {
         //Add back button to the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Basic textView to test the bottom navigation
-        TextView title = (TextView) findViewById(R.id.testTextAddRecipe);
-        title.setText("THIS IS THE ADD RECIPE ACTIVITY");
+        //Set the rating spinner to show numbers from 1-5
+        Spinner ratingMenu = findViewById(R.id.ratingMenu);
+        //Every possible value of the spinner
+        Integer[] ratingLevels = new Integer[]{1,2,3,4,5};
+        //Set an adapter for the spinner
+        ArrayAdapter<Integer> adapterRating = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, ratingLevels);
+        //Set the style for the spinner menu
+        adapterRating.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ratingMenu.setAdapter(adapterRating);
+        //Upon clicking the spinner, the user can choose the difficulty level from the ratingLevels array
+        ratingMenu.setOnItemSelectedListener(this);
+
     }
 
     //Pressing the back button on the action bar will take the user back to the previous activity
@@ -33,5 +49,16 @@ public class ActivityAddRecipe extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //The difficulty rating will be saved to an integer value when clicked
+        Integer rating = (int) parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
