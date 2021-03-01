0b30d56a259c2fb20e3f9892884314ef898b2059
tamask <tamask@metropolia.fi>
2021-03-01 20:27:06 +0200

    added functionality for the "ADD INGREDIENT" button

--- app/src/main/java/fi/group6/dessertrecipeapp/ActivityAddRecipe.java
+++ app/src/main/java/fi/group6/dessertrecipeapp/ActivityAddRecipe.java
@@ -12,18 +12,24 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-//TODO UI - make "ADD INGREDIENT" button work
 //TODO - configure "ADD RECIPE" button
 //TODO - add recipe tag selection to the UI and the code
 //TODO - figure out how to work with photos
 
-public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    LinearLayout ingredientListLayout;
+    Button addIngredient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,11 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
         getSupportActionBar().setTitle("Add recipe");
         //Add back button to the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ingredientListLayout = findViewById(R.id.ingredient_list);
+        addIngredient = findViewById(R.id.addIngredientButton);
+
+        addIngredient.setOnClickListener(this);
 
         //Set the rating spinner to show numbers from 1-5
         Spinner ratingMenu = findViewById(R.id.ratingMenu);
@@ -48,6 +59,8 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
         ratingMenu.setOnItemSelectedListener(this);
 
     }
+
+
 
     //Pressing the back button on the action bar will take the user back to the previous activity
     @Override
@@ -65,5 +78,39 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //Ingredient row is added when the user clicks the "ADD INGREDIENT" button
+        addIngredientRow();
+    }
+
+    private void addIngredientRow() {
+
+        //Inflate the ingredients row by one
+        View ingredientRow = getLayoutInflater().inflate(R.layout.add_ingredient_row, null, false);
+
+        //Connect the widgets inside the add_ingredient_row.xml with the code
+        EditText ingredientAmount = (EditText)ingredientRow.findViewById(R.id.ingredientAmount);
+        EditText ingredientMeasure = (EditText)ingredientRow.findViewById(R.id.ingredientMeasure);
+        EditText ingredientName = (EditText)ingredientRow.findViewById(R.id.ingredientName);
+        ImageView deleteIngredientRow = (ImageView)ingredientRow.findViewById(R.id.image_delete);
+
+        //If the user clicks the delete button, that specific row will be deleted
+        deleteIngredientRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeRow(ingredientRow);
+            }
+        });
+
+        ingredientListLayout.addView(ingredientRow);
+    }
+
+    private void removeRow(View view) {
+        //When the user clicks the delete button, the ingredient row will be deleted
+        ingredientListLayout.removeView(view);
     }
 }
