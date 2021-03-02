f0e2cca18070fc03db62c69674cf324c94e086b4
tamask <tamask@metropolia.fi>
2021-03-02 14:35:15 +0200

    working on ActivityAddRecipe functionalities

--- app/src/main/java/fi/group6/dessertrecipeapp/ActivityAddRecipe.java
+++ app/src/main/java/fi/group6/dessertrecipeapp/ActivityAddRecipe.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +23,10 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //TODO - configure "ADD RECIPE" button
 //TODO - add recipe tag selection to the UI and the code
 //TODO - figure out how to work with photos
@@ -30,8 +35,25 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
 
     LinearLayout ingredientListLayout;
     LinearLayout instructionListLayout;
-    Button addIngredient;
-    Button addInstruction;
+
+    EditText name;
+    EditText author;
+    EditText portions;
+    EditText prepTime;
+
+    Button addIngredientButton;
+    Button addInstructionButton;
+    Button addRecipeButton;
+
+    String nameInput;
+    String step;
+    String authorInput;
+    String difficultyRating;
+
+    Integer portionInput;
+    Integer prepTimeInput;
+
+    List<String> instructions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +66,20 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ingredientListLayout = findViewById(R.id.ingredient_list);
-        addIngredient = findViewById(R.id.addIngredientButton);
+        addIngredientButton = findViewById(R.id.addIngredientButton);
 
         instructionListLayout = findViewById(R.id.instruction_list);
-        addInstruction = findViewById(R.id.addInstructionsButton);
+        addInstructionButton = findViewById(R.id.addInstructionsButton);
+        addRecipeButton = findViewById(R.id.addRecipeButton);
 
-        addIngredient.setOnClickListener(this);
-        addInstruction.setOnClickListener(this);
+        name = findViewById(R.id.editTextTextPersonName);
+        author = findViewById(R.id.author);
+        portions = findViewById(R.id.portionSize);
+        prepTime = findViewById(R.id.prepTime);
+
+        addIngredientButton.setOnClickListener(this);
+        addInstructionButton.setOnClickListener(this);
+        addRecipeButton.setOnClickListener(this);
 
         //Set the rating spinner to show numbers from 1-5
         Spinner ratingMenu = (Spinner) findViewById(R.id.ratingMenu);
@@ -95,7 +124,88 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
             case R.id.addInstructionsButton:
                 addInstructionRow();
                 break;
+            case R.id.addRecipeButton:
+                if(checkdataValidity()){
+                    addNewRecipeWithIngredients();
+                }
+                break;
         }
+    }
+
+    private void addNewRecipeWithIngredients() {
+
+        nameInput = name.getText().toString();
+        authorInput = author.getText().toString();
+        portionInput = Integer.parseInt(portions.getText().toString());
+        prepTimeInput = Integer.parseInt(prepTime.getText().toString());
+
+        Spinner ratingMenu = (Spinner) findViewById(R.id.ratingMenu);
+        difficultyRating = ratingMenu.getSelectedItem().toString();
+
+        Log.d("Rating is", difficultyRating);
+
+        for(int i = 0; i < instructionListLayout.getChildCount(); i++){
+
+            View instructionStepLayout = instructionListLayout.getChildAt(i);
+            EditText instructionStep = (EditText)instructionStepLayout.findViewById(R.id.instructions);
+            step = instructionStep.getText().toString();
+
+            if(!instructionStep.getText().toString().equals("")){
+                instructions.add(step);
+            }
+        }
+
+    }
+
+    private boolean checkdataValidity() {
+
+        boolean result = true;
+
+        if(name.getText().toString().equals("")){
+            Toast.makeText(ActivityAddRecipe.this,
+                    "Please enter a recipe name", Toast.LENGTH_LONG).show();
+        }
+
+        //INGREDIENTS GO HERE
+
+        if(portions.getText().toString().equals("0") || portions.getText().toString().equals("00") ||
+                portions.getText().toString().equals("000") || portions.getText().toString().equals("")){
+            Toast.makeText(ActivityAddRecipe.this,
+                    "Please enter a correct portion size", Toast.LENGTH_LONG).show();
+            result = false;
+        }
+
+        if(prepTime.getText().toString().equals("0") || prepTime.getText().toString().equals("00") ||
+                prepTime.getText().toString().equals("000") || prepTime.getText().toString().equals("")){
+            Toast.makeText(ActivityAddRecipe.this,
+                    "Please enter a correct preparation time", Toast.LENGTH_LONG).show();
+            result = false;
+        }
+
+        if(instructionListLayout.getChildCount() == 0){
+            Toast.makeText(ActivityAddRecipe.this,
+                    "Please add at least one instruction step", Toast.LENGTH_LONG).show();
+            result = false;
+        }
+
+        for(int i = 0; i < instructionListLayout.getChildCount(); i++){
+
+            View instructionStepLayout = instructionListLayout.getChildAt(i);
+            EditText instructionStep = (EditText)instructionStepLayout.findViewById(R.id.instructions);
+
+            if(instructionStep.getText().toString().equals("")) {
+                Toast.makeText(ActivityAddRecipe.this,
+                        "Please fill out or delete the the empty instruction texts", Toast.LENGTH_LONG).show();
+                result = false;
+            }
+        }
+
+        if(author.getText().toString().equals("")){
+            Toast.makeText(ActivityAddRecipe.this,
+                    "Please enter an author name", Toast.LENGTH_LONG).show();
+        }
+
+        return result;
     }
 
     private void addIngredientRow() {
@@ -113,7 +223,7 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
         deleteIngredientRow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                removeRow(ingredientRow);
+                removeIngredientRow(ingredientRow);
             }
         });
         ingredientListLayout.addView(ingredientRow);
@@ -132,18 +242,18 @@ public class ActivityAddRecipe extends AppCompatActivity implements AdapterView.
         deleteInstructionRow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                removeInsRow(instructionRow);
+                removeInstructionRow(instructionRow);
             }
         });
         instructionListLayout.addView(instructionRow);
     }
 
-    private void removeRow(View view) {
+    private void removeIngredientRow(View view) {
         //When the user clicks the delete button, the ingredient row will be deleted
         ingredientListLayout.removeView(view);
     }
 
-    private void removeInsRow(View view) {
+    private void removeInstructionRow(View view) {
         //When the user clicks the delete button, the ingredient row will be deleted
         instructionListLayout.removeView(view);
     }
